8ae91d7cf0d232347a6f270e8295dfa39498ec44
omer9977 <63466920+omer9977@users.noreply.github.com>
2023-03-14 00:42:27 +0300

    docker done

--- Presentation/VotingAPI.WebAPI/Program.cs
+++ Presentation/VotingAPI.WebAPI/Program.cs
@@ -58,9 +58,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors();
 
