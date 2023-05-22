d3797ee045f7dc47e7ef8c6963a5caf5160747f6
Ömer Yılmaz <omerisler20@gmail.com>
2023-05-22 21:18:34 +0300

    test removed

--- Presentation/VotingAPI.Tests/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using VotingAPI.ObsService.Interfaces;
-using VotingAPI.ObsService.Services;
-using VotingAPI.Tests;
-
-var studentService = new ObsStudentService();
-
-var json = studentService.FindUserByUserName("metinince@std.iyte.edu.tr");
-
-var string1 = "";
