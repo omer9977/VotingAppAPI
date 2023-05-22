614a4aedb66fdcaa5e08ba7e3685ded9959e1012
Ömer Yılmaz <omerisler20@gmail.com>
2023-05-23 00:03:41 +0300

    done

--- Core/VotingAPI.Application/Dto/Request/User/LoginUserRequest.cs
+++ Core/VotingAPI.Application/Dto/Request/User/LoginUserRequest.cs
@@ -8,7 +8,7 @@ namespace VotingAPI.Application.Dto.Request.User
 {
     public class LoginUserRequest
     {
-        public string Email { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
     }
 }
